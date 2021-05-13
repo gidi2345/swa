d96f0d2ab364bebe7cff345884132c44fab2caf8
gidi2345 <gidon1smila@gmail.com>
2021-05-13 13:36:27 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import CountUp from 'react-countup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{display:'flex',position:'fixed', alignItems:'center',height:'10vh' ,width:'100%', background:'black'}}>
+        <div style={{fontSize:'1.5rem', color: 'white', marginLeft:'3%'}}>
+          #StandWithUs
+        </div>
+      </div>
+      <div style={{display:'flex', justifyContent:'center', alignItems:'center',flexDirection:'column', height: '90vh', width:'100%',background: '#a30b18'}}>
+        <div style={{fontSize:'2.3rem', color:'white', display:'flex', justifyContent:'center', alignItems:'center'}}>Missiles on Israel in the last 72 hours</div>
+      <CountUp style={{fontSize: '10rem', color:'white'}} end={1300} />
+      </div>
+      <div style={{display:'flex', justifyContent:'center', alignItems:'center',flexDirection:'column', height: '90vh', width:'100%',background: '#a30b18'}}>
+        <div style={{fontSize:'2.3rem', color:'white', display:'flex', justifyContent:'center', alignItems:'center'}}>Missiles on America and Europe in the last 25 fucking years</div>
+      <CountUp style={{fontSize: '10rem', color:'white'}} end={0} />
+      </div>
     </div>
   );
 }
